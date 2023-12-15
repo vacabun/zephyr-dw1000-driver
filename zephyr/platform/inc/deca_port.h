7d9e85a186afc21dac355e16e3367654cb5969d5
guotong ma <maguotong66@gmail.com>
2023-12-15 15:25:11 +0800

    update

--- zephyr/platform/inc/deca_port.h
+++ zephyr/platform/inc/deca_port.h
@@ -18,12 +18,11 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 decaIrqStatus_t decamutexon(void);
 void decamutexoff(decaIrqStatus_t s);
 void deca_usleep(unsigned long time_us);
 void deca_sleep(unsigned int time_ms);
-int usleep(unsigned long usec);
-void Sleep(uint32_t x);
 
 #ifdef __cplusplus
 }
--- zephyr/platform/src/deca_port.c
+++ zephyr/platform/src/deca_port.c
@@ -14,6 +14,7 @@ void decamutexoff(decaIrqStatus_t s)
     // TODO?: s is not used
     dw1000_hw_interrupt_enable();
 }
+
 void deca_sleep(unsigned int time_ms)
 {
     k_msleep(time_ms);
@@ -22,13 +23,4 @@ void deca_sleep(unsigned int time_ms)
 void deca_usleep(unsigned long time_us)
 {
     k_usleep(time_us);
-}
-int usleep(unsigned long usec)
-{
-    k_usleep(usec);
-}
-
-void Sleep(uint32_t x)
-{
-    k_msleep(x);
 }
